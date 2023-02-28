7fb55bc75198c73774cc0b021cca0c12695cb0bb
Mar <bezpontov@i.ua>
2023-02-28 17:26:58 +0200

    pom file modified TestOneClass modified TestTwoClass modified testng_testone added testng_testtwo added

--- src/test/java/TestOneClass.java
+++ src/test/java/TestOneClass.java
@@ -1,2 +1,11 @@
+import org.testng.annotations.Test;
+
+@Test
 public class TestOneClass {
+
+    @Test
+    public void PrintSomeText(){
+        System.out.println("Test One Some Text.");
+    }
+
 }
--- src/test/java/TestTwoClass.java
+++ src/test/java/TestTwoClass.java
@@ -1,2 +1,11 @@
+import org.testng.annotations.Test;
+
+@Test
 public class TestTwoClass {
+
+    @Test
+    public void PrintSomeText(){
+        System.out.println("Test Two Some Text.");
+    }
+
 }
